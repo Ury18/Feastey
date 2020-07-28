9027ba2f3ff993b84be7186f797dd7defa97fc5d
Ury18 <ury_98@hotmail.com>
2020-07-28 18:48:25 +0200

    Fixed maps api key

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withImages = require('next-images');
 
 module.exports = withCss(withSass(withImages({
     env: {
-        GOOGLE_MAPS_KEY: "AIzaSyDCL0Ym4mgltetgeA91KCtQ4ZPH62zdq - 8",
+        GOOGLE_MAPS_KEY: "AIzaSyDCL0Ym4mgltetgeA91KCtQ4ZPH62zdq-8",
         GOOGLE_MAPS_MAP_URL: "https://maps.googleapis.com/maps/api/js?",
         GOOGLE_MAPS_DIRECTIONS_URL: "https://www.google.com/maps/dir/",
         GOOGLE_MAPS_GEOCODE_URL: "https://maps.googleapis.com/maps/api/geocode/json?",
