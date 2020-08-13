278329c57a8d6877dcb2edf0842b6ca7de43724c
Ury18 <ury_98@hotmail.com>
2020-08-14 00:25:37 +0200

    Added mobile menu navigation

--- app/components/MainNav/index.js
+++ app/components/MainNav/index.js
@@ -11,7 +11,7 @@ const MainNav = ((props) => {
     const router = useRouter()
     const [registerBoxActive, setRegisterBoxActive] = useState(false)
     const [accountBoxActive, setAccountBoxActive] = useState(false)
-
+    const [navActive, setNavActive] = useState(false)
     const { updateUserData } = props
 
     useEffect(() => {
@@ -74,53 +74,103 @@ const MainNav = ((props) => {
     }
 
     return (
-        <nav className="mainNav">
-            <ul>
-                {!props.user.token && <li>
-                    <button id="registerButtonBox" onClick={(e) => registerButtonClick(e)}>Registrarse</button>
-                    {registerBoxActive && <ul id="registerBox">
-                        <li>
-                            <Link href="/signup">
-                                <a className={router.pathname == "/signup" ? "active" : ""}>Usuario</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/business-signup">
-                                <a className={router.pathname == "/business-signup" ? "active" : ""}>Profesional</a>
-                            </Link>
-                        </li>
-                    </ul>}
-                </li>}
-                {!props.user.token && <li>
-                    <Link href="/login">
-                        <a className={router.pathname == "/login" ? "active" : ""}>Iniciar Sesión</a>
-                    </Link>
-                </li>}
-                {props.user.token && <li>
-                    <button id="accountBoxButton" onClick={(e) => accountButtonClick(e)}>{props.user.username}</button>
-                    {accountBoxActive && <ul id="accountBox">
-                        <li>
-                            <Link href={`/users/edit`}>
-                                <a className={router.pathname == '/users/edit' ? "active" : ""}>Información de la cuenta</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={`/users/favorites`}>
-                                <a className={router.pathname == '/users/favorites' ? "active" : ""}>Favoritos</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={`/users/my-businesses`}>
-                                <a className={router.pathname == '/users/my-businesses' ? "active" : ""}>Mis negocios</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <button onClick={(e) => onExit(e)}>Salir</button>
-                        </li>
-                    </ul>}
-                </li>}
-            </ul>
-        </nav>
+        <div>
+            <nav className="mainNav">
+                <ul>
+                    {!props.user.token && <li>
+                        <button id="registerButtonBox" onClick={(e) => registerButtonClick(e)}>Registrarse</button>
+                        {registerBoxActive && <ul id="registerBox">
+                            <li>
+                                <Link href="/signup">
+                                    <a className={router.pathname == "/signup" ? "active" : ""}>Usuario</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/business-signup">
+                                    <a className={router.pathname == "/business-signup" ? "active" : ""}>Profesional</a>
+                                </Link>
+                            </li>
+                        </ul>}
+                    </li>}
+                    {!props.user.token && <li>
+                        <Link href="/login">
+                            <a className={router.pathname == "/login" ? "active" : ""}>Iniciar Sesión</a>
+                        </Link>
+                    </li>}
+                    {props.user.token && <li>
+                        <button id="accountBoxButton" onClick={(e) => accountButtonClick(e)}>{props.user.username}</button>
+                        {accountBoxActive && <ul id="accountBox">
+                            <li>
+                                <Link href={`/users/edit`}>
+                                    <a className={router.pathname == '/users/edit' ? "active" : ""}>Información de la cuenta</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/users/favorites`}>
+                                    <a className={router.pathname == '/users/favorites' ? "active" : ""}>Favoritos</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/users/my-businesses`}>
+                                    <a className={router.pathname == '/users/my-businesses' ? "active" : ""}>Mis negocios</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <button onClick={(e) => onExit(e)}>Salir</button>
+                            </li>
+                        </ul>}
+                    </li>}
+                </ul>
+            </nav>
+            <i className="fas fa-bars button-mobile" aria-hidden="true" onClick={e => setNavActive(!navActive)}/>
+            <nav className={`mainNav mobile ${navActive ? "active" : ""}`}>
+                <i className="closeMenu fas fa-times" onClick={(e) => setNavActive(false)}></i>
+                <ul>
+                    {!props.user.token && <li>
+                        <label>Registrarse</label>
+                        <ul id="registerBox">
+                            <li>
+                                <Link href="/signup">
+                                    <a className={router.pathname == "/signup" ? "active" : ""}>Usuario</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/business-signup">
+                                    <a className={router.pathname == "/business-signup" ? "active" : ""}>Profesional</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </li>}
+                    {!props.user.token && <li>
+                        <Link href="/login">
+                            <a className={router.pathname == "/login" ? "active" : ""}>Iniciar Sesión</a>
+                        </Link>
+                    </li>}
+                    {props.user.token && <li>
+                        <ul id="accountBox">
+                            <li>
+                                <Link href={`/users/edit`}>
+                                    <a className={router.pathname == '/users/edit' ? "active" : ""}>Información de la cuenta</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/users/favorites`}>
+                                    <a className={router.pathname == '/users/favorites' ? "active" : ""}>Favoritos</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={`/users/my-businesses`}>
+                                    <a className={router.pathname == '/users/my-businesses' ? "active" : ""}>Mis negocios</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <button onClick={(e) => onExit(e)}>Salir</button>
+                            </li>
+                        </ul>
+                    </li>}
+                </ul>
+            </nav>
+        </div>
     )
 })
 
