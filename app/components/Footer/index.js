bd7960d9aa5bcb074ca3e9c85a4e4a3a06eb2ea1
Ury18 <ury_98@hotmail.com>
2020-08-17 17:09:54 +0200

    Footer 1st iteration

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,12 +1,28 @@
 import Link from 'next/link'
-import MainNav from '../MainNav'
 import './index.scss'
 
 export default ((props) => {
 
     return (
         <footer className="footer">
-            <p>Footer</p>
+            <Link href='/'>
+                <a className="home-link">
+                    <img src="/img/feastey_logo.png" />
+                </a>
+            </Link>
+            <div>
+                <Link href='/privacy-policy'>
+                    <a>
+                        Politica de Privacidad
+                    </a>
+                </Link>
+                <Link href='/terms-and-conditions'>
+                    <a>
+                        Terminos y Condiciones
+                    </a>
+                </Link>
+                <a href="mailto:feastey@feastey.com">feastey@feastey.com</a>
+            </div>
         </footer>
     )
 })
