08bf6d0975f71e6b2922189cb650a0e0466679ee
Ury18 <ury_98@hotmail.com>
2020-08-18 23:13:48 +0200

    Added missing categories on boilerplate

--- api/scripts/feastey-boilerplate.js
+++ api/scripts/feastey-boilerplate.js
@@ -29,14 +29,126 @@ module.exports = async () => {
         delete admin.password
         console.log("Admin Created")
 
-        let restaurantCategory = {
-            name:"Restauración"
-        }
 
-        let restauracion = new Category(restaurantCategory)
+        let alimentacionCategory = {
+            name: "Almientación"
+        }
+        let alimentacion = new Category(alimentacionCategory)
+        await alimentacion.save()
+
+
+        let drogueriaCategory = {
+            name: "Droguería y Perfumería"
+        }
+        let drogueria = new Category(drogueriaCategory)
+        await drogueria.save()
+
+
+        let electroCategory = {
+            name: "Electrodomésticos y Tecnología"
+        }
+        let electro = new Category(electroCategory)
+        await electro.save()
+
+
+        let esteticaCategory = {
+            name: "Estética"
+        }
+        let estetica = new Category(esteticaCategory)
+        await estetica.save()
+
+
+        let ferreteriaCategory = {
+            name: "Ferretería y Bricolaje"
+        }
+        let ferreteria = new Category(ferreteriaCategory)
+        await ferreteria.save()
+
+
+        let jardinCategory = {
+            name: "Floristería y Jardinería"
+        }
+        let jardin = new Category(jardinCategory)
+        await jardin.save()
+
+
+        let hogarCategory = {
+            name: "Hogar, Muebles y Menaje"
+        }
+        let hogar = new Category(hogarCategory)
+        await hogar.save()
+
+
+        let inmobiliariaCategory = {
+            name: "Inmobiliaria"
+        }
+        let inmobiliaria = new Category(inmobiliariaCategory)
+        await inmobiliaria.save()
+
+
+        let joyeriaCategory = {
+            name: "Joyería y Relojería"
+        }
+        let joyeria = new Category(joyeriaCategory)
+        await joyeria.save()
+
+
+        let modaCategory = {
+            name: "Moda"
+        }
+        let moda = new Category(modaCategory)
+        await moda.save()
+
+
+        let ocioCategory = {
+            name: "Ocio"
+        }
+        let ocio = new Category(ocioCategory)
+        await ocio.save()
+
+
+        let papeleriaCategory = {
+            name: "Papelería y Librería"
+        }
+        let papeleria = new Category(papeleriaCategory)
+        await papeleria.save()
+
+
+        let restauracionCategory = {
+            name:"Restauración y Bares"
+        }
+        let restauracion = new Category(restauracionCategory)
         await restauracion.save()
 
-        console.log("Categoria de restauración creada")
+
+        let saludCategory = {
+            name: "Salud"
+        }
+        let salud = new Category(saludCategory)
+        await salud.save()
+
+
+        let serviciosCategory = {
+            name: "Servicios"
+        }
+        let servicios = new Category(serviciosCategory)
+        await servicios.save()
+
+
+        let talleresCategory = {
+            name: "Talleres"
+        }
+        let talleres = new Category(talleresCategory)
+        await talleres.save()
+
+
+        let otrosCategory = {
+            name: "Otros"
+        }
+        let otros = new Category(otrosCategory)
+        await otros.save()
+
+        console.log("Todas las categorias han sido creadas")
 
     }
 }
