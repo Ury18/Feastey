e90a2e55083b8a0f15a9cc0dcb69c3c508f2be07
Ury18 <ury_98@hotmail.com>
2020-08-18 20:04:43 +0200

    Final footer fix

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -5,23 +5,25 @@ export default ((props) => {
 
     return (
         <footer className="footer">
-            <Link href='/'>
-                <a className="home-link">
-                    <img src="/img/feastey_logo.png" />
-                </a>
-            </Link>
             <div>
-                <Link href='/privacy-policy'>
-                    <a>
-                        Politica de Privacidad
+                <Link href='/'>
+                    <a className="home-link">
+                        <img src="/img/feastey_logo.png" />
                     </a>
                 </Link>
-                <Link href='/terms-and-conditions'>
-                    <a>
-                        Terminos y Condiciones
+                <div>
+                    <Link href='/privacy-policy'>
+                        <a>
+                            Politica de Privacidad
                     </a>
-                </Link>
-                <a href="mailto:feastey@feastey.com">feastey@feastey.com</a>
+                    </Link>
+                    <Link href='/terms-and-conditions'>
+                        <a>
+                            Terminos y Condiciones
+                    </a>
+                    </Link>
+                    <a href="mailto:feastey@feastey.com">feastey@feastey.com</a>
+                </div>
             </div>
         </footer>
     )
