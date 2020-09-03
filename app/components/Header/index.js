f99422113693da340a266346e5798629726d4ef3
Ury18 <ury_98@hotmail.com>
2020-09-03 22:40:12 +0200

    Added Discover button to header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -7,7 +7,13 @@ export default ((props) => {
     return (
         <header className="header">
             <Link href="/">
-                <a><img className="logo" alt="Feastey Logo"src={"/img/feastey_logo.png"}></img></a>
+                <a className="logo-link"><img className="logo" alt="Feastey Logo"src={"/img/feastey_logo.png"}></img></a>
+            </Link>
+            <Link href="/business">
+                <a className="discover_button button">
+                    <i className="fas fa-search-location discover_icon" />
+                    Descubre tu zona
+                </a>
             </Link>
             <MainNav/>
         </header>
